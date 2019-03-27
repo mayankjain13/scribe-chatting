3897d467cc1e5e353d8c0c98f910b6eba65ef725
“mayankjain13” <mayankraysoni@gmail.com>
2019-03-27 15:55:46 +0530

    first-commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAXXzY8NpSPB2dH4ppQs9_5OmRcX4vRSTU',
+    authDomain: 'scribe-efe48.firebaseapp.com',
+    databaseURL: 'https://scribe-efe48.firebaseio.com',
+    projectId: 'scribe-efe48',
+    storageBucket: 'scribe-efe48.appspot.com',
+    messagingSenderId: '310495142831'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAXXzY8NpSPB2dH4ppQs9_5OmRcX4vRSTU',
+    authDomain: 'scribe-efe48.firebaseapp.com',
+    databaseURL: 'https://scribe-efe48.firebaseio.com',
+    projectId: 'scribe-efe48',
+    storageBucket: 'scribe-efe48.appspot.com',
+    messagingSenderId: '310495142831'
+  }
+};
